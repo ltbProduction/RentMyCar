38b605290c0032c94be8ab5d58733affdc7f9007
lukasblunck96 <49271087+lukasblunck96@users.noreply.github.com>
2019-11-14 10:44:37 +0100

    neu

---
+++ js-rental.js
@@ -0,0 +1,21 @@
+
+var name = document.querySelector("#name").value;
+
+
+let makeCar = function(name, ort, mail, phone, brand, type, fuel, description, price) {
+
+    console.log(name, ort, mail, phone, brand, type, fuel, description, price);
+
+    let car = {
+        name = this.name;
+        ort = this.ort;
+        mail = this.mail;
+        phone = this.phone;
+        brand = this.brand;
+        type = this.type;
+        fuel = this.fuel;
+        description = this.description;
+        price = this.price;
+    }
+}
+
