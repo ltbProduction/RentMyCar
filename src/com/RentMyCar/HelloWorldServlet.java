257c4ad8d85db471d7054a33a744d19e23cef1f5
robinbirkhofer <birkhofer_r@topflow.local>
2019-11-15 10:32:58 +0100

    backup erweiterung update

--- src/com/RentMyCar/HelloWorldServlet.java
+++ src/com/RentMyCar/HelloWorldServlet.java
@@ -1,8 +1,21 @@
 package com.RentMyCar;
 
-public class HelloWorldServlet {
+import java.io.IOException;
 
-	
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		;
+		
+		System.out.println(req.getParameter("message"));
+	}
 	
 	
 }
