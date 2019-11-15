9093d0c215b81b0f275d297a84c55de26130e583
lukasblunck96 <49271087+lukasblunck96@users.noreply.github.com>
2019-11-15 12:35:34 +0100

    jQuery in js-rental

--- WebContent/js-rental.js
+++ WebContent/js-rental.js
@@ -1,28 +1,27 @@
 
 
-
-//let makeCar = function(name, city, mail, phone, brand, type, fuel, description, price) {
-/*function emailveri(email) {
+function emailveri(email) {
     return (email.search(/^w+((-w+)|(.w+))*@[A-Za-z0-9]+((.|-)[A-Za-z0-9]+)*.[A-Za-z0-9]+$/) != -1);
-}*/
+}
 
 function printCar() {
-    let name = document.querySelector("#name").value;
-    let city = document.querySelector("#city").value;
-    let email = document.querySelector("#email").value;
-   
-/*if (!emailveri(email.value)) {
+    let name = $('#name').value;
+    let city = $('#city').value;
+    let email = $('#email').value;
+
+if (!emailveri(email)) {
     alert('Bitte gültige E-Mail-Adresse angeben!');
     field.focus();
     return false;
-}*/
-    let phone = document.querySelector("#phone").value;
-    let brand = document.querySelector("#brand").value;
-    let type = document.querySelector("#type").value;
-    let fuel = document.querySelector("#fuel").value;
-    let price = document.querySelector("#price").value;
-    let description = document.querySelector("#description").value;
-    let image = document.querySelector("#image").value;
+}
+
+    let phone = $('#phone').value;;
+    let brand = $('#brand').value;;
+    let type = $('#type').value;;
+    let fuel = $('#fuel').value;;
+    let price = $('#price').value;;
+    let description = $('#description').value;;
+    let image = $('#image').value;
     console.log(name, city, email, phone, brand, type, fuel, description, image, price);
 
 }
