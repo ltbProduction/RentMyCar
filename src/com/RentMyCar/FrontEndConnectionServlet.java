7757816571f139e8d2f64739cbbb7fd460c24473
ltbProduction <49271109+ltbProduction@users.noreply.github.com>
2019-11-22 16:29:20 +0100

    anpassung hello-world in new-element

--- src/com/RentMyCar/FrontEndConnectionServlet.java
+++ src/com/RentMyCar/FrontEndConnectionServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class FrontEndConnectionServlet {
+public class FrontEndConnectionServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String message = req.getParameter("message");
