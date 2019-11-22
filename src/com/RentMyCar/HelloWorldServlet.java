346e43a394d38a7b51ba921b344351b1b3fba37e
lukasblunck96 <49271087+lukasblunck96@users.noreply.github.com>
2019-11-22 21:46:49 +0100

    Card in car_overview.html

--- src/com/RentMyCar/HelloWorldServlet.java
+++ src/com/RentMyCar/HelloWorldServlet.java
@@ -5,7 +5,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
