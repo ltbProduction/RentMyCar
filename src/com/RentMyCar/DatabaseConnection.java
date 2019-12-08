7cb67181dd45e4efd46baebcf48bd5e1678886be
lukasblunck96 <49271087+lukasblunck96@users.noreply.github.com>
2019-12-08 11:53:00 +0100

    push branch

--- src/com/RentMyCar/DatabaseConnection.java
+++ src/com/RentMyCar/DatabaseConnection.java
@@ -1,9 +1,8 @@
 package com.RentMyCar;
 
-
-
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -11,12 +10,10 @@ import java.util.ArrayList;
 
 import org.apache.jasper.tagplugins.jstl.core.ForEach;
 
-
-
 public class DatabaseConnection {
 	private static String url = "jdbc:mysql://localhost:3306/rentmycar?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=Europe/Berlin";
-	 
-	//Ab Hier ignorieren
+
+	// Ab Hier ignorieren
 	public static void main(String[] args) throws ClassNotFoundException {
 //		 CarElement c1 = new CarElement("Linus Brugger", "Ravensburg", "linus@brugger-rv.de", "01709123866", "Audi", "A4", "Diesel", "Tolles Auto", 1.3);
 //		 CarElement c2 = new CarElement("Robin Birkhofer", "Ravensburg", "was", "01709123866", "Renault", "Twingo", "Benzin", "Schei� Auto", -3);
@@ -28,41 +25,82 @@ public class DatabaseConnection {
 		ArrayList<CarElement> cars = new ArrayList<>();
 		cars = readfromDatabase();
 //		FrontEndConnectionServlet.writecarelements(DatabaseConnection.readfromDatabase());
-		for(CarElement c : cars){
+		for (CarElement c : cars) {
 			System.out.println(c.getFuel());
 			System.out.println(c.getName());
 		}
-			
-		}
-	//Bis hier
-	 
-	
-	public static void writeToDatabase(CarElement c1) throws ClassNotFoundException {
-		try {
-			//Class.forName("com.mysql.jdbc");
-			Connection conn = DriverManager.getConnection(url, "root", "RentMyCar2019");
-			Statement statement = conn.createStatement();
-			String datac1;
-			datac1 = String.valueOf(c1.getElement_id())+ ",";
-			datac1 = datac1 + "'"+c1.getName()+"',";
-			datac1 = datac1 + "'"+c1.getLocation()+"',";
-			datac1 = datac1 + "'"+c1.getMail()+"',";			
-			datac1 = datac1 + "'"+c1.getPhonenumber()+"',";
-			datac1 = datac1 + "'"+c1.getBrand()+"',";
-			datac1 = datac1 + "'"+c1.getType()+"',";
-			datac1 = datac1 + "'"+c1.getFuel()+"',";
-			datac1 = datac1 + "'"+String.valueOf(c1.getPrice())+"',";
-			datac1 = datac1 + "'"+c1.getDescription()+"'";
-			
-			String insert = "insert into carelement(element_id, persName, location, email, phonenumber, carbrand, cartyp, fueltyp, price, description_text) values ("+datac1+")";
-			
-			statement.executeUpdate(insert);
-		
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+
 	}
-	
+	// Bis hier
+
+	public static void writeToDatabase(CarElement c1) throws ClassNotFoundException {
+		Connection conn = null;
+		String message = null;
+		try {
+			// Class.forName("com.mysql.jdbc");
+			conn = DriverManager.getConnection(url, "root", "RentMyCar2019");
+			// Statement statement = conn.createStatement();
+
+			// constructs SQL statement
+			/*String datac1;
+			datac1 = String.valueOf(c1.getElement_id()) + ",";
+			datac1 = datac1 + "'" + c1.getName() + "',";
+			datac1 = datac1 + "'" + c1.getLocation() + "',";
+			datac1 = datac1 + "'" + c1.getMail() + "',";
+			datac1 = datac1 + "'" + c1.getPhonenumber() + "',";
+			datac1 = datac1 + "'" + c1.getBrand() + "',";
+			datac1 = datac1 + "'" + c1.getType() + "',";
+			datac1 = datac1 + "'" + c1.getFuel() + "',";
+			datac1 = datac1 + "'" + String.valueOf(c1.getPrice()) + "',";
+			datac1 = datac1 + "'" + c1.getDescription() + "'";*/
+
+			String insert = "INSERT INTO carelement(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+			PreparedStatement statement = conn.prepareStatement(insert);
+			statement.setString(1, String.valueOf(c1.getElement_id()));
+			statement.setString(2, c1.getName());
+			statement.setString(3, c1.getLocation());
+			statement.setString(4, c1.getMail());
+			statement.setString(5, c1.getPhonenumber());
+			statement.setString(6, c1.getBrand());
+			statement.setString(7, c1.getType());
+			statement.setString(8, c1.getFuel());
+			statement.setString(9, String.valueOf(c1.getPrice()));
+			statement.setString(10, c1.getDescription());
+
+			if (c1.getPhoto() != null) {
+				// fetches input stream of the upload file for the blob column
+				statement.setBlob(11, c1.getPhoto());
+			}
+
+			// sends the statement to the database server
+			int row = statement.executeUpdate();
+			if (row > 0) {
+				message = "File uploaded and saved into database";
+			}
+
+		} catch (SQLException e) {
+			message = "ERROR: " + e.getMessage();
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				// closes the database connection
+				try {
+					conn.close();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+
+		}
+
+	}
+	// sets the message in request scope
+	// req.setAttribute("Message", message);
+
+	// forwards to the message page
+	// getServletContext().getRequestDispatcher("/Message.jsp").forward(request,
+	// response);
+
 	public static ArrayList<CarElement> readfromDatabase(){
 		ArrayList<CarElement> cars = new ArrayList<CarElement>();
 		 try
@@ -95,7 +133,8 @@ public class DatabaseConnection {
 								rs.getString("cartyp"), 
 								rs.getString("fueltyp"),
 								rs.getString("description_text"),
-								rs.getDouble("price"));
+								rs.getDouble("price"),
+		    					rs.getBinaryStream("photo"));
 								
 		    	ce.setCreated_at(rs.getDate("created_at"));
 		        cars.add(ce);
@@ -112,9 +151,5 @@ public class DatabaseConnection {
 			return cars;
 	  
 	}
-
-	
-	
-
 
 }
--- src/com/RentMyCar/FrontEndConnection.java
+++ src/com/RentMyCar/FrontEndConnection.java
@@ -1,161 +1,162 @@
 package com.RentMyCar;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.tomcat.jni.File;
+//import org.apache.tomcat.jni.File;
+
+import javax.servlet.http.Part;
 
 /**
  * Servlet implementation class HalloServlet
  */
 @WebServlet("/HalloServlet")
+@MultipartConfig(maxFileSize = 16177215)
 public class FrontEndConnection extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FrontEndConnection() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	// database connection settings
+	// private String dbURL = "jdbc:mysql://localhost:3306/AppDB";
+	// private String dbUser = "root";
+	// private String dbPass = "secret";
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-        ServletOutputStream out = response.getOutputStream();
-        
-        out.println("<html>");
-        out.println("<head>" +"<link rel=\"icon\" type=\"image/vnd.microsoft.icon\" href=\"img\\car_logo.jpg\">\r\n" + 
-        		"  <meta charset=\"utf-8\">\r\n" + 
-        		"  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">\r\n" + 
-        		"  <meta name=\"description\" content=\"\">\r\n" + 
-        		"  <meta name=\"author\" content=\"\">"
-        		+ "<title>RentMyCar</title>" + "  <link href=\"vendor/bootstrap/css/bootstrap.min.css\" rel=\"stylesheet\">\r\n" + 
-        				"  <link href=\"css/the-big-picture.css\" rel=\"stylesheet\">\r\n" + 
-        		        		
-        		"</head>");
- 
-        out.println("<body>");
-        out.println(" <nav class=\"navbar navbar-expand-lg navbar-dark bg-dark fixed-top>\">\r\n" + 
-        		"    <div class=\"container\">\r\n" + 
-        		"      <a class=\"navbar-brand\" href=\"#\">Rent My Car</a>\r\n" + 
-        		"      <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarResponsive\" aria-controls=\"navbarResponsive\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\r\n" + 
-        		"        <span class=\"navbar-toggler-icon\"></span>\r\n" + 
-        		"      </button>\r\n" + 
-        		"      <div class=\"collapse navbar-collapse\" id=\"navbarResponsive\">\r\n" + 
-        		"        <ul class=\"navbar-nav ml-auto\">\r\n" + 
-        		"          <li class=\"nav-item\">\r\n" + 
-        		"            <a class=\"nav-link\" href=\"index.html\">Home\r\n" + 
-        		"              <span class=\"sr-only\">(current)</span>\r\n" + 
-        		"            </a>\r\n" + 
-        		"          </li>\r\n" + 
-        		"          <li class=\"nav-item active\">\r\n" + 
-        		"            <a class=\"nav-link\" href=\"cars\">Unsere Autos</a>\r\n" + 
-        		"          </li>\r\n" + 
-        		"          <li class=\"nav-item\">\r\n" + 
-        		"            <a class=\"nav-link\" href=\"contacts.html\">Stellen Sie ihr Auto online</a>\r\n" + 
-        		"          </li>\r\n" + 
-        		"          <li class=\"nav-item\">\r\n" + 
-        		"            <a class=\"nav-link\" href=\"legal_notice.html\">Impressum</a>\r\n" + 
-        		"          </li>\r\n" + 
-        		"        </ul>\r\n" + 
-        		"      </div>\r\n" + 
-        		"    </div>\r\n" + 
-        		"  </nav>");
-        out.println("<section>\r\n" + 
-        		"    <div class=\"container\">\r\n" + 
-        		"      <div class=\"row\">\r\n" + 
-        		"        <div class=\"col-lg-6\">\r\n" + 
-        		"          <h1 class=\"mt-5\">Autos</h1>\r\n" + 
-        		"          <section>\r\n" +  
-        		"        </section>");
-		ArrayList<CarElement> cars = DatabaseConnection.readfromDatabase();
-        
-        for (CarElement c : cars  ) {
-            
-            out.println("<div class=\"card\" style=\"width: flex;\">\r\n" + 
-            		"  <img src=\"img\\1559835101_mst.jpg\" class=\"card-img-top\" alt=\"...\">\r\n" + 
-            		"  <div class=\"card-body\">\r\n" + 
-            		"    <h5 class=\"card-title\">"+c.getBrand()+" "+c.getType()+"</h5>\r\n" + 
-            		"    <p class=\"card-text\">\r\n" + 
-            		"		<strong>Name:</strong> "+c.getName() +"\r\n" + 
-            		"            <br>\r\n" + 
-            		"            <strong>Ort:</strong> "+c.getLocation()+"\r\n" + 
-            		"            <br>\r\n" + 
-            		"            <strong>Telefon:</strong> "+c.getPhonenumber()+"\r\n" + 
-            		"            <br>\r\n" + 
-            		"            <strong>E-Mail:</strong> "+c.getMail()+"\r\n" + 
-            		"            <br>\r\n" + 
-            		"            <strong>Automarke:</strong> "+c.getBrand()+"\r\n" + 
-            		"            <br>\r\n" + 
-            		"            <strong>Autotyp:</strong> "+c.getType()+"\r\n" + 
-            		"            <br>\r\n" + 
-            		"            <strong>Kraftstoff:</strong> "+c.getFuel()+"\r\n" + 
-            		"            <br>\r\n" + 
-            		"            <strong>Preis pro Stunde:</strong> "+c.getPrice()+" EUR\r\n" + 
-            		"            <br>	\r\n" + 
-            		"            <strong>Beschreibung:</strong> "+c.getDescription()+" \r\n" + 
-            		"            <br>	\r\n" + 
-            		"	</p>\r\n" + 
-            		"    <a href=\"mailto:"+c.getMail()+"?subject=RentMyCar%20Autovermietung&amp;body=Hallo%20"+c.getName()+",%0D%0A%0D%0Aich%20wollte%20Ihnen%20mitteilen,%20dass%20ich%20Interesse%20an%20Ihrem%20Auto%20"+c.getBrand()+"%20"+c.getType()+"%20habe.%20Bitte%20geben%20Sie%20mir%20eine%20Rueckmeldung,%20ob%20das%20Auto%20aktuell%20zur%20Verfuegung%20steht.%0D%0A%0D%0AViele%20Gruesse"+"\" class=\"btn btn-primary\">Mieten</a>\r\n" + 
-            		"  </div>\r\n" + 
-            		"</div>"
-            		+ "<br>");
-		}
-
-        
-
-        out.println("       </div>\r\n" + 
-        		"      </div>\r\n" + 
-        		"    </div>\r\n" + 
-        		"  </section>");
-        out.println(" <script src=\"vendor/jquery/jquery.min.js\"></script>\r\n" + 
-        		"  <script src=\"vendor/bootstrap/js/bootstrap.bundle.min.js\"></script>\r\n" + 
-        		"\r\n" + 
-        		"</body>\r\n" + 
-        		"\r\n" + 
-        		"</html>");
+	public FrontEndConnection() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		int element_id = DatabaseConnection.readfromDatabase().size()+1;
-		CarElement car1 = new CarElement(
-				element_id,
-				request.getParameter("name"), 
-				request.getParameter("location"), 
-				request.getParameter("email"),
-				request.getParameter("phone"), 
-				request.getParameter("brand"),
-				request.getParameter("type"),
-				request.getParameter("fuel"),
-				request.getParameter("description"),
-				Double.parseDouble(request.getParameter("price")));
-		//Speicher bild
-//		File image = new File();
-//		image = request.getParameter("image");
-		
-		
+		ServletOutputStream out = response.getOutputStream();
+
+		out.println("<html>");
+		out.println("<head>" + "<link rel=\"icon\" type=\"image/vnd.microsoft.icon\" href=\"img\\car_logo.jpg\">\r\n"
+				+ "  <meta charset=\"utf-8\">\r\n"
+				+ "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">\r\n"
+				+ "  <meta name=\"description\" content=\"\">\r\n" + "  <meta name=\"author\" content=\"\">"
+				+ "<title>RentMyCar</title>"
+				+ "  <link href=\"vendor/bootstrap/css/bootstrap.min.css\" rel=\"stylesheet\">\r\n"
+				+ "  <link href=\"css/the-big-picture.css\" rel=\"stylesheet\">\r\n" +
+
+				"</head>");
+
+		out.println("<body>");
+		out.println(" <nav class=\"navbar navbar-expand-lg navbar-dark bg-dark fixed-top>\">\r\n"
+				+ "    <div class=\"container\">\r\n" + "      <a class=\"navbar-brand\" href=\"#\">Rent My Car</a>\r\n"
+				+ "      <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarResponsive\" aria-controls=\"navbarResponsive\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\r\n"
+				+ "        <span class=\"navbar-toggler-icon\"></span>\r\n" + "      </button>\r\n"
+				+ "      <div class=\"collapse navbar-collapse\" id=\"navbarResponsive\">\r\n"
+				+ "        <ul class=\"navbar-nav ml-auto\">\r\n" + "          <li class=\"nav-item\">\r\n"
+				+ "            <a class=\"nav-link\" href=\"index.html\">Home\r\n"
+				+ "              <span class=\"sr-only\">(current)</span>\r\n" + "            </a>\r\n"
+				+ "          </li>\r\n" + "          <li class=\"nav-item active\">\r\n"
+				+ "            <a class=\"nav-link\" href=\"cars\">Unsere Autos</a>\r\n" + "          </li>\r\n"
+				+ "          <li class=\"nav-item\">\r\n"
+				+ "            <a class=\"nav-link\" href=\"contacts.html\">Stellen Sie ihr Auto online</a>\r\n"
+				+ "          </li>\r\n" + "          <li class=\"nav-item\">\r\n"
+				+ "            <a class=\"nav-link\" href=\"legal_notice.html\">Impressum</a>\r\n"
+				+ "          </li>\r\n" + "        </ul>\r\n" + "      </div>\r\n" + "    </div>\r\n" + "  </nav>");
+		out.println("<section>\r\n" + "    <div class=\"container\">\r\n" + "      <div class=\"row\">\r\n"
+				+ "        <div class=\"col-lg-6\">\r\n" + "          <h1 class=\"mt-5\">Autos</h1>\r\n"
+				+ "          <section>\r\n" + "        </section>");
+		ArrayList<CarElement> cars = DatabaseConnection.readfromDatabase();
+
+		for (CarElement c : cars) {
+
+			out.println("<div class=\"card\" style=\"width: flex;\">\r\n"
+					+ "  <img src=\"img\\1559835101_mst.jpg\" class=\"card-img-top\" alt=\"...\">\r\n"
+					+ "  <div class=\"card-body\">\r\n" + "    <h5 class=\"card-title\">" + c.getBrand() + " "
+					+ c.getType() + "</h5>\r\n" + "    <p class=\"card-text\">\r\n" + "		<strong>Name:</strong> "
+					+ c.getName() + "\r\n" + "            <br>\r\n" + "            <strong>Ort:</strong> "
+					+ c.getLocation() + "\r\n" + "            <br>\r\n" + "            <strong>Telefon:</strong> "
+					+ c.getPhonenumber() + "\r\n" + "            <br>\r\n" + "            <strong>E-Mail:</strong> "
+					+ c.getMail() + "\r\n" + "            <br>\r\n" + "            <strong>Automarke:</strong> "
+					+ c.getBrand() + "\r\n" + "            <br>\r\n" + "            <strong>Autotyp:</strong> "
+					+ c.getType() + "\r\n" + "            <br>\r\n" + "            <strong>Kraftstoff:</strong> "
+					+ c.getFuel() + "\r\n" + "            <br>\r\n" + "            <strong>Preis pro Stunde:</strong> "
+					+ c.getPrice() + " EUR\r\n" + "            <br>	\r\n"
+					+ "            <strong>Beschreibung:</strong> " + c.getDescription() + " \r\n"
+					+ "            <br>	\r\n" + "	</p>\r\n" + "    <a href=\"mailto:" + c.getMail()
+					+ "?subject=RentMyCar%20Autovermietung&amp;body=Hallo%20" + c.getName()
+					+ ",%0D%0A%0D%0Aich%20wollte%20Ihnen%20mitteilen,%20dass%20ich%20Interesse%20an%20Ihrem%20Auto%20"
+					+ c.getBrand() + "%20" + c.getType()
+					+ "%20habe.%20Bitte%20geben%20Sie%20mir%20eine%20Rueckmeldung,%20ob%20das%20Auto%20aktuell%20zur%20Verfuegung%20steht.%0D%0A%0D%0AViele%20Gruesse"
+					+ "\" class=\"btn btn-primary\">Mieten</a>\r\n" + "  </div>\r\n" + "</div>" + "<br>");
+		}
+
+		out.println("       </div>\r\n" + "      </div>\r\n" + "    </div>\r\n" + "  </section>");
+		out.println(" <script src=\"vendor/jquery/jquery.min.js\"></script>\r\n"
+				+ "  <script src=\"vendor/bootstrap/js/bootstrap.bundle.min.js\"></script>\r\n" + "\r\n" + "</body>\r\n"
+				+ "\r\n" + "</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		InputStream inputStream = null; // input stream of the upload file
+
+		// obtains the upload file part in this multipart request
+		Part filePart = request.getPart("image");
+		if (filePart != null) {
+			// prints out some information for debugging
+			System.out.println(filePart.getName());
+			System.out.println(filePart.getSize());
+			System.out.println(filePart.getContentType());
+
+			// obtains input stream of the upload file
+			inputStream = filePart.getInputStream();
+		}
+
+		int element_id = DatabaseConnection.readfromDatabase().size() + 1;
+		CarElement car1 = new CarElement(element_id, request.getParameter("name"), request.getParameter("location"),
+				request.getParameter("email"), request.getParameter("phone"), request.getParameter("brand"),
+				request.getParameter("type"), request.getParameter("fuel"), request.getParameter("description"),
+				Double.parseDouble(request.getParameter("price")), inputStream);
+
+		Connection conn = null; // connection to the database
+		//String message = null; // message will be sent back to client
+
 		try {
 			DatabaseConnection.writeToDatabase(car1);
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				// closes the database connection
+				try {
+					conn.close();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+			doGet(request, response);
 		}
-		doGet(request, response);
+
 	}
 
 }
